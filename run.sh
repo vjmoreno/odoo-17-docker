332175d7fb131fee2df5b6a16e1897f8c87fb276
vjmoreno <vicente.moreno@psgrupohunting.cl>
2024-11-07 20:26:39 -0300

    feat(docker config)

---
+++ run.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+
+docker-compose up -d --build
+
+# Create logs folder if it does not exist
+mkdir -p logs
+
+# Max size in bytes
+MAX_SIZE=$((10 * 1024 * 1024)) 
+
+# Verify and rotate log if its necessary
+if [[ -f logs/app.log && $(stat -c%s logs/app.log) -ge $MAX_SIZE ]]; then
+    mv logs/app.log logs/app-$(date +"%Y%m%d%H%M%S").log  # Rename file
+    touch logs/app.log  # Create new log file
+fi
+
+docker-compose logs -f >> logs/app.log &
+echo "Odoo and PostgreSQL containers successfully created"
